f8aa3cf197ea8ba5e386066dc68fd84ab3cd4734
jmiller-boundless <jmiller@boundlessgeo.com>
2018-02-17 17:44:56 -0500

    added arc and style

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,17 @@ import Map from 'ol/map';
 import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
 import XYZ from 'ol/source/xyz';
+import Style from 'ol/style/style'
+import Stroke from 'ol/style/stroke'
+import Arc from 'arc'
+
+new Style({
+  stroke: new Stroke({
+  color: '#EAE911',
+  width: 2
+  })
+
+});
 
 new Map({
   target: 'map',
